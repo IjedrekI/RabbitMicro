cff1b451475175efd89c5a9e7278fffdc7353051
Andrzej Witas <Andrzej.Witas@mdsl.com>
2022-02-17 12:33:04 +0000

    Init

---
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroRabbit.Domain.Core.Bus;
+
+public interface IEventHandler<in TEvent> : IEventHandler
+    where TEvent : Event.Event
+{
+    Task Handle(TEvent @event);
+}
+
+public interface IEventHandler { }
